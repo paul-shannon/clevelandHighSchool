e513c032cf7423a0860b3174f4490e47a6672e64
paul-shannon <paul.thurmond.shannon@gmail.com>
2016-02-17 10:49:28 -0800

    runCleveland now tailored for eager.systemsbiology.org

--- webapps/current/runCleveland.R
+++ webapps/current/runCleveland.R
@@ -1,13 +1,13 @@
 library(ChinookServer)
 library(RUnit)
-PORT=6092
+PORT=7001
 datasets <- "SouthSeattleHealthImpacts"
 analysisPackages <- c("ChinookLinearModel")
 browserFile <- "index.html"
 userCredentials <- "test@nowhere.net"
 chinook <- ChinookServer(port=PORT, analysisPackages, datasets, browserFile, userCredentials)
 
-if(Sys.info()[["nodename"]] != "lopez")
+if(Sys.info()[["nodename"]] != "eager.systemsbiology.net")
    browseURL(sprintf("http://localhost:%d", PORT))
 
 run(chinook)

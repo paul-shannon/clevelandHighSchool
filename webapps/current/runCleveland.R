6e442fae65be98d6d5ea7356c3d2e0ee6b5480ae
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-05-02 12:17:21 -0700

    restored to working order, about to be dockerized

--- webapps/current/runCleveland.R
+++ webapps/current/runCleveland.R
@@ -1,5 +1,18 @@
 library(ChinookServer)
 library(RUnit)
+
+stopifnot(packageVersion("ChinookLinearModel") >= "1.0.3")
+stopifnot(packageVersion("Groups") >= "1.0.6")
+stopifnot(packageVersion("LinearModel") >= "1.1.5")
+stopifnot(packageVersion("ChinookAnalysis") >= "1.0.1")
+stopifnot(packageVersion("SouthSeattleHealthImpacts") >= "1.0.5")
+stopifnot(packageVersion("Dataset") >= "1.0.1")
+stopifnot(packageVersion("SubjectHistory") >= "1.0.2")
+stopifnot(packageVersion("ChinookServer") >= "1.0.12")
+stopifnot(packageVersion("ChinookDataset") >= "1.0.8")
+stopifnot(packageVersion("httpuv") >= "1.4.1")
+stopifnot(packageVersion("jsonlite") >= "1.5")
+
 PORT=7001
 datasets <- "SouthSeattleHealthImpacts"
 analysisPackages <- c("ChinookLinearModel")
